0bac8a5ef59a4f748e97c7a76616e0a8bf0e89a8
Chris Thunes <cmthunes@ncsu.edu>
2010-02-25 16:31:47 -0500

    Add defaults for logging configuration

--- db/seawolf_db_init.sql
+++ db/seawolf_db_init.sql
@@ -3,10 +3,14 @@ BEGIN TRANSACTION;
 
 CREATE TABLE config (option VARCHAR PRIMARY KEY, value VARCHAR);
 INSERT INTO "config" VALUES('password','');
+INSERT INTO "config" VALUES('log_file','seawolf.log');
 
 CREATE TABLE variables (id INT AUTO_INCREMENT UNIQUE PRIMARY KEY, time TIMESTAMP, precisetime DOUBLE, name CHAR(20), value FLOAT);
 
 CREATE TABLE variable_definitions (name CHAR(64) UNIQUE PRIMARY KEY, default_value DOUBLE, persistent BOOL, readonly BOOL);
+INSERT INTO "variable_definitions" VALUES('LogLevel',2,0,1);
+INSERT INTO "variable_definitions" VALUES('LogReplicateStdout',2,0,0);
+
 INSERT INTO "variable_definitions" VALUES('SEA.Roll',0.0,0,0);
 INSERT INTO "variable_definitions" VALUES('SEA.Yaw',0.0,0,0);
 INSERT INTO "variable_definitions" VALUES('SEA.Pitch',0.0,0,0);
