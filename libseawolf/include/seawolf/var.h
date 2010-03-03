418e2535e8f49c3da899e2ca2bbf22cfe3f9b29d
Chris Thunes <cmthunes@ncsu.edu>
2010-03-03 12:07:55 -0500

    Remove unneeded prototypes

--- libseawolf/include/seawolf/var.h
+++ libseawolf/include/seawolf/var.h
@@ -5,10 +5,8 @@
 #ifndef __SEAWOLF_VAR_INCLUDE_H
 #define __SEAWOLF_VAR_INCLUDE_H
 
-void Var_init(void);
 float Var_get(char* name);
 void Var_setAutoNotify(bool autonotify);
 void Var_set(char* name, float value);
-void Var_close(void);
 
 #endif // #ifndef __SEAWOLF_VAR_INCLUDE_H
