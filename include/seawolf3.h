2142e46edbea3bdffaff436df0f48c43ed70df04
Chris Thunes <cmthunes@ncsu.edu>
2010-05-26 22:57:48 -0400

    Add shared include file to replace constants in the hub

---
+++ include/seawolf3.h
@@ -0,0 +1,17 @@
+/**
+ * \file 
+ * \brief Seawolf III shared
+ *
+ * Definitions and enumerations which are shared by different Seawolf III
+ * applications
+ */
+
+#ifndef __SEAWOLF3_INCLUDE_H
+#define __SEAWOLF3_INCLUDE_H
+
+#define THRUSTER_MAX 63
+
+#define ROT_MODE_ANGULAR 0x01
+#define ROT_MODE_RATE    0x02
+
+#endif // #ifndef __SEAWOLF3_H_
