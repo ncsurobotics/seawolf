df6bf97b8969f6711869b4b81b6755116646d094
Michael Brown <msbrown3@ncsu.edu>
2010-05-28 18:14:24 +0100

    More comprehensive zerothrusters.c.

--- applications/src/reakonmission.c
+++ applications/src/reakonmission.c
@@ -9,9 +9,16 @@ int main(void) {
     //Notify_get(NULL, NULL);
 
     Notify_send("THRUSTER_REQUEST", Util_format("Forward %d %d", 0, 0));
-    Var_set("Rot.Angular.Target", 50.0);
-    Var_set("DepthHeading", 10.0);
-    //Notify_send("THRUSTER_REQUEST", Util_format("Forward %d %d", 40, 40));
+    //Var_set("Rot.Angular.Target", 50.0);
+    //Var_set("DepthHeading", 10.0);
+
+    // Go Forward
+    Notify_send("THRUSTER_REQUEST", Util_format("Forward %d %d", 20, 20));
+
+    // Zero yaw pid
+    float current_yaw = Var_get("SEA.Yaw");
+    Var_set("Rot.Mode", 1.0);
+    Var_set("Rot.Angular.Target", current_yaw);
 
     Seawolf_close();
     return 0;
--- applications/src/zerothrusters.c
+++ applications/src/zerothrusters.c
@@ -5,12 +5,19 @@ int main(void) {
     Seawolf_loadConfig("../conf/seawolf.conf");
     Seawolf_init("Thruster Controller");
 
+    Notify_send("THRUSTER_REQUEST", Util_format("Forward %d %d", 0, 0));
+
     /* Turn off drivers */
     Var_set("PortX", 0);
     Var_set("PortY", 0);
     Var_set("StarX", 0);
     Var_set("StarY", 0);
     Var_set("Aft", 0);
+
+    // Zero yaw pid
+    float current_yaw = Var_get("SEA.Yaw");
+    Var_set("Rot.Mode", 1.0);
+    Var_set("Rot.Angular.Target", current_yaw);
 
     Seawolf_close();
     return 0;
