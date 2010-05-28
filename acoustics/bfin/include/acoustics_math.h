fb69bcd40f297d13aee5a16467624be02c89fda4
Chris Thunes <cmthunes@ncsu.edu>
2010-05-28 18:03:43 -0400

    Acoustics updates, lots of optimizations and a bit of house keeping

--- acoustics/bfin/include/acoustics_math.h
+++ acoustics/bfin/include/acoustics_math.h
@@ -9,14 +9,15 @@
 void flip(fract16*, int);
 void fft(fract16*, complex_fract16*, int);
 void ifft(complex_fract16*, fract16*, int);
-void firfly(fract16*, int, fir_state_fr16*);
-void multiply(complex_fract16*, complex_fract16*, complex_fract16*, int);
+void fir_inplace(fract16*, int, fir_state_fr16*);
+void fir(fract16*, fract16*, int, fir_state_fr16*);
 void convolve(fract16*, fract16*, fract16*, int);
-void fast_convolve(fract16*, fract16*, fract16*, int);
 void correlate(fract16*, fract16*, fract16*, int);
 void fast_correlate(fract16*, fract16*, fract16*, int);
-int findMax(fract16* f, int size);
-
-#define ARRAY_SIZE 8192
+int find_max(fract16* f, int size);
+int find_max_cmplx(complex_fract16* f, int size);
+void conjugate(complex_fract16* input, int size);
+void multiply(complex_fract16* in1, complex_fract16* in2, complex_fract16* out, int size);
+void real_part(complex_fract16* input, fract16* output, int size);
 
 #endif // #ifndef CSV_IN_H
--- acoustics/bfin/src/acoustics_math.c
+++ acoustics/bfin/src/acoustics_math.c
@@ -23,30 +23,20 @@ void flip(fract16* data, int size) {
     }
 }
 
-void multiply(complex_fract16* in1, complex_fract16* in2, complex_fract16* out, int size) {
-    while(size--) {
-        *out++ = cmlt_fr16(*in1++, *in2++);
-    }
-}
-
 void fft(fract16* input, complex_fract16* output, int size) {
     /* Create space for twiddle table */
     complex_fract16* tt;
     int block_exponent;
 
-    printf(" Allocating buffers\n");
     tt = malloc(sizeof(complex_fract16) * (size / 2));
 
     /* Populate twiddle table */
-    printf(" Setting up twiddle table\n");
     twidfftrad2_fr16(tt, size);
 
     /* Execute FFT */
-    printf(" Doing FFT\n");
     rfft_fr16(input, output, tt, 1, size, &block_exponent, 1);
 
     /* Free twiddle table */
-    printf(" Freeing\n");
     free(tt);
 }
 
@@ -59,20 +49,16 @@ void ifft(complex_fract16* cmplx, fract16* data, int size) {
     complex_fract16* tt;
     complex_fract16* temp_out;
 
-    printf(" Allocating buffers\n");
     tt = malloc(sizeof(complex_fract16) * tt_size);
     temp_out = malloc(sizeof(complex_fract16) * size);
 
     /* Set up twiddle table */
-    printf(" Setting up twiddle table\n");
     twidfftrad2_fr16(tt, size);
 
     /* Execute IFFT */
-    printf(" Doing IFFT\n");
     ifft_fr16(cmplx, temp_out, tt, 1, size, &block_exponent, 1);
 
     /* Convert complex to real */
-    printf(" Converting complex to real\n");
     for(int i = 0; i < size; i++) {
         /* This works, data is a local copy of the data pointer being passed to it */
         *(data++) = temp_out[i].re;
@@ -83,7 +69,7 @@ void ifft(complex_fract16* cmplx, fract16* data, int size) {
 }
 
 /* FIR filter (send it down the zipline) */
-void firfly(fract16* data, int size, fir_state_fr16* firState) {
+void fir_inplace(fract16* data, int size, fir_state_fr16* firState) {
     fract16* tempData;
 
     /* Create Temporary Data array */
@@ -99,35 +85,12 @@ void firfly(fract16* data, int size, fir_state_fr16* firState) {
     free(tempData);
 }
 
-void convolve(fract16* f, fract16*g, fract16* out, int size) {
-    convolve_fr16(f, size, g, size, out);
+void fir(fract16* input, fract16* output, int size, fir_state_fr16* fir_state) {
+    fir_fr16(input, output, size, fir_state);
 }
 
-void fast_convolve(fract16* f, fract16* g, fract16* out, int size) {
-    /* Implements fast convolution */
-    complex_fract16* f_fft;
-    complex_fract16* g_fft;
-    complex_fract16* out_fft;
-
-    f_fft = malloc(sizeof(complex_fract16) * size);
-    g_fft = malloc(sizeof(complex_fract16) * size);
-    out_fft = malloc(sizeof(complex_fract16) * size);
-
-    printf("FFT 1\n");
-    fft(f, f_fft, size);
-
-    printf("FFT 2\n");
-    fft(g, g_fft, size);
-
-    printf("Multiply\n");
-    multiply(f_fft, g_fft, out_fft, size);
-
-    printf("IFFT\n");
-    ifft(out_fft, out, size);
-
-    free(f_fft);
-    free(g_fft);
-    free(out_fft);
+void convolve(fract16* f, fract16*g, fract16* out, int size) {
+    convolve_fr16(f, size, g, size, out);
 }
 
 void correlate(fract16* f, fract16* g, fract16* out, int size) {
@@ -136,19 +99,75 @@ void correlate(fract16* f, fract16* g, fract16* out, int size) {
     convolve(f, g, out, size);
 }
 
-void fast_correlate(fract16* f, fract16* g, fract16* out, int size) {
-    /* Cross Correlation = Convolution(f(-t), g(t)) */
-    flip(f, size);
-    fast_convolve(f, g, out, size);
+void conjugate(complex_fract16* input, int size) {
+    for(int i = 0; i < size; i++) {
+        input[i] = conj_fr16(input[i]);
+    }
 }
 
-int findMax(fract16* f, int size) {
+void multiply(complex_fract16* in1, complex_fract16* in2, complex_fract16* out, int size) {
+    while(size--) {
+        *out++ = cmlt_fr16(*in1++, *in2++);
+    }
+}
+
+void real_part(complex_fract16* input, fract16* output, int size) {
+    for(int i = 0; i < size; i++) {
+        *(output++) = (input++)->re;
+    }
+}
+
+void fast_correlate(fract16* f, fract16* g, fract16* out, int size) {
+    /* Cross correlation = ifft(fft(f)* . fft(g))
+       Where the asterisk denotes the complex conjegate
+       
+       See http://en.wikipedia.org/wiki/Cross_correlation */
+
+    complex_fract16* f_fft;
+    complex_fract16* g_fft;
+
+    f_fft = malloc(sizeof(complex_fract16) * size);
+    g_fft = malloc(sizeof(complex_fract16) * size);
+
+    /* Compute both fourier transforms */
+    fft(f, f_fft, size);
+    fft(g, g_fft, size);
+
+    /* Conjugate the fourier transform of f */
+    conjugate(f_fft, size);
+
+    /* Multiply the transforms and store result back to f_fft */
+    multiply(f_fft, g_fft, f_fft, size);
+
+    /* Compute the inverse fourier transform. This output is the correlation of
+       f and g */
+    ifft(f_fft, out, size);
+
+    free(f_fft);
+    free(g_fft);
+}
+
+int find_max(fract16* f, int size) {
     fract16 max_y = 0;
     int max_x = 0;
 
     for(int i = 0; i < size; i++) {
         if(f[i] > max_y) {
             max_y = f[i];
+            max_x = i;
+        } 
+    }
+
+    return max_x;
+}
+
+int find_max_cmplx(complex_fract16* f, int size) {
+    fract16 max_y = 0;
+    int max_x = 0;
+
+    for(int i = 0; i < size; i++) {
+        if(f[i].re > max_y) {
+            max_y = f[i].re;
             max_x = i;
         } 
     }
