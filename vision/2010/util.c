ea26d7ae2fc094c3c6441080489603d90c4e67db
Michael Brown <msbrown3@ncsu.edu>
2010-06-13 21:41:41 +0100

    PixToDeg is is fixed.  I don't know why it was set to pass through values before.

--- vision/2010/util.c
+++ vision/2010/util.c
@@ -13,7 +13,6 @@
 //convert pixels from center into degrees from center
 float PixToDeg(int p){
     float degrees;
-    //degrees = floor( 6.49606e-10*pow(p,5) - 2.47052e-7*pow(p,4) + 0.0000336612*pow(p,3) - 0.00205154*p*p + 0.190667*p );
-    degrees = p;
+    degrees = floor( (6.49606e-10)*pow(p,5) - (2.47052e-7)*pow(p,4) + 0.0000336612*pow(p,3) - 0.00205154*p*p + 0.190667*p );
     return degrees;
 }
