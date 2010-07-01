82a48db43e971dcd593c859e1f946cd061af32ef
Chris Thunes <cmthunes@ncsu.edu>
2010-07-01 00:22:25 -0400

    Serial driver and serialapp updates for new thruster board

---
+++ serial/src/drivers/ard/thruster_board.c
@@ -0,0 +1,78 @@
+
+#include "seawolf.h"
+
+#include <math.h>
+#include <string.h>
+
+/* Minimum value to turn thrusters on at */
+#define DEAD_BAND 8
+
+/* Thruster numbers */
+#define PORT_X 0
+#define STAR_X 1
+#define PORT_Y 2
+#define STAR_Y 3
+#define AFT    4
+
+void manage(SerialPort sp);
+
+const char* app_name = "Serial : ThrusterBoard";
+
+static int get_thruster_number(const char* name) {
+    if(strcmp(name, "PortX") == 0) {
+        return PORT_X;
+    } else if(strcmp(name, "StarX") == 0) {
+        return STAR_X;
+    } else if(strcmp(name, "PortY") == 0) {
+        return PORT_Y;
+    } else if(strcmp(name, "StarY") == 0) {
+        return STAR_Y;
+    } else if(strcmp(name, "AFT") == 0) {
+        return AFT;
+    }
+
+    return -1;
+}
+
+void manage(SerialPort sp) {
+    /* Receive all thruster update notifications */
+    Notify_filter(FILTER_MATCH, "UPDATED PortX");
+    Notify_filter(FILTER_MATCH, "UPDATED StarX");
+    Notify_filter(FILTER_MATCH, "UPDATED PortY");
+    Notify_filter(FILTER_MATCH, "UPDATED StarY");
+    Notify_filter(FILTER_MATCH, "UPDATED Aft");
+
+    char varname[32];
+    unsigned char data[2];
+    float value;
+
+    /* Main loop */
+    while(true) {
+        Notify_get(NULL, varname);
+
+        /* Get thruster number */
+        data[0] = get_thruster_number(varname);
+
+        /* Get thruster value */
+        value = (int) Var_get(varname);
+
+        /* Set base value */
+        data[1] = (int) fabs(value);
+        
+        /* Set to zero if less than dead band */
+        if(data[1] <= DEAD_BAND) {
+            data[1] = 0;
+        }
+
+        /* Set direction bit */
+        if(value < 0) {
+            data[1] |= (1 << 6);
+        }
+
+        /* Set high bit (used as frame sync) */
+        data[1] |= (1 << 7);
+
+        /* Send data */
+        Serial_send(sp, data, 2);
+    }
+}
--- serial/src/serialapp.c
+++ serial/src/serialapp.c
@@ -17,6 +17,7 @@ typedef enum{PT_UNMANAGED,
              PT_THRUSTER12,
              PT_THRUSTER3,
              PT_THRUSTER45,
+             PT_THRUSTER_BOARD,
              PT_MISSIONSTATUS} PeripheralType;
 
 struct comm_device {
@@ -116,6 +117,8 @@ static int getPeripheralType(SerialPort sp) {
                 return PT_THRUSTER3;
             } else if(strcmp(id, "Thruster45") == 0) {
                 return PT_THRUSTER45;
+            } else if(strcmp(id, "ThrusterBoard") == 0) {
+                return PT_THRUSTER_BOARD;
             } else if(strcmp(id, "MissionStatus") == 0) {
                 return PT_MISSIONSTATUS;
             } else {
@@ -154,13 +157,14 @@ int main(void) {
     const int device_count = sizeof(device_pool) / sizeof(struct comm_device);
 
     /* App to executable mappings */
-    struct comm_assignment app_pool[] = {[PT_DEPTH]         = {"./bin/depth",          false},
-                                         [PT_THRUSTER12]    = {"./bin/thruster12-bin", false},
-                                         [PT_THRUSTER3]     = {"./bin/thruster3-bin",  false},
-                                         [PT_THRUSTER45]    = {"./bin/thruster45-bin", false},
-                                         [PT_ALTIMETER]     = {"./bin/altimeter",      false},
-                                         [PT_IMU]           = {"./bin/imu",            false},
-                                         [PT_MISSIONSTATUS] = {"./bin/status",         false}};
+    struct comm_assignment app_pool[] = {[PT_DEPTH]          = {"./bin/depth",          false},
+                                         [PT_THRUSTER12]     = {"./bin/thruster12-bin", false},
+                                         [PT_THRUSTER3]      = {"./bin/thruster3-bin",  false},
+                                         [PT_THRUSTER45]     = {"./bin/thruster45-bin", false},
+                                         [PT_THRUSTER_BOARD] = {"./bin/thruster_board", false},
+                                         [PT_ALTIMETER]      = {"./bin/altimeter",      false},
+                                         [PT_IMU]            = {"./bin/imu",            false},
+                                         [PT_MISSIONSTATUS]  = {"./bin/status",         false}};
     const int app_count = sizeof(app_pool) / sizeof(struct comm_assignment);
 
     int i = 0;
