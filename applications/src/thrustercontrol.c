b4f3f27985543d6b913277acd131d6cb847fc78f
Chris Thunes <cmthunes@ncsu.edu>
2011-01-20 23:39:50 -0500

    Fix thruster control

--- applications/src/thrustercontrol.c
+++ applications/src/thrustercontrol.c
@@ -5,9 +5,9 @@
 #include <stdio.h>
 #include <ncurses.h>
 
-#define MIN_SPEED (-60)
-#define MAX_SPEED (60)
-#define STEP (5)
+#define MIN_SPEED (-1.0)
+#define MAX_SPEED (1.0)
+#define STEP (0.05)
 
 #define ADJUST(x) Util_inRange(MIN_SPEED, (x), MAX_SPEED)
 
@@ -31,19 +31,20 @@ int main(void) {
          %3.2f          \n\
 ";
 
-    float bow, stern, port, star, strafe;
+    float bow=0.0, stern=0.0, port=0.0, star=0.0, strafe=0.0;
     int addsub = 1;
         
     /* Turn off drivers */
-    Var_set("Port", 0);
-    Var_set("Star", 0);
-    Var_set("Bow", 0);
-    Var_set("Stern", 0);
-    Var_set("Strafe", 0);
-
-    while(1) {
-        printw(display, bow, port, strafe, star, bow);
+    Var_set("Port", 0.0);
+    Var_set("Star", 0.0);
+    Var_set("Bow", 0.0);
+    Var_set("Stern", 0.0);
+    Var_set("Strafe", 0.0);
+    
+    while(true) {
+        printw(display, bow, port, strafe, star, stern);
         refresh();
+
         c = getch();
         clear();
 
@@ -55,21 +56,25 @@ int main(void) {
 
         switch(c) {
         case 'w':
-            port = ADJUST(bow + (addsub * STEP));
+            bow = ADJUST(bow + (addsub * STEP));
             Var_set("Bow", bow);
             break;
+
         case 'x':
             stern = ADJUST(stern + (addsub * STEP));
             Var_set("Stern", stern);
             break;
+
         case 'a':
             port = ADJUST(port + (addsub * STEP));
             Var_set("Port", port);
             break;
+
         case 'd':
             star = ADJUST(star + (addsub * STEP));
             Var_set("Star", star);
             break;
+
         case 's':
             strafe = ADJUST(strafe + (addsub * STEP));
             Var_set("Strafe", strafe);
