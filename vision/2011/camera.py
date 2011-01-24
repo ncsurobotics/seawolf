7fe0fcb0be9cb74f68ae05120eb31ddb92b78209
Michael Brown <msbrown3@ncsu.edu>
2011-01-24 16:42:34 -0500

    Static image support for Camera class.

--- vision/2011/camera.py
+++ vision/2011/camera.py
@@ -2,50 +2,64 @@
 import cv
 
 class Camera(object):
-    '''An wrapper for OpenCV's camera functionality.'''
+    '''An wrapper for OpenCV's camera functionality.
 
-    def __init__(self, index, display=True, record=True):
+    Supports camera indexes, video files, and image files.
+
+    '''
+
+    def __init__(self, identifier, display=True, record=True):
         '''
         Arguments:
 
-        index - This is a capture device indentifier suitable to be passed to
-            cv.CaptureFromFile() (if index is a string) or cv.CaptureFromCAM()
-            (if index is an int).  Typically this argument should be a filename
-            of a video file, or an index of a camera.
+        indentifier - This is a capture device indentifier suitable to be
+            passed to cv.CaptureFromFile() (if indentifier is a string) or
+            cv.CaptureFromCAM() (if indentifier is an int).  This argument
+            should be a filename of a video file, an index of a camera, or a
+            filename of an image.
 
         display - If True, a graphical window will display the images captured
             from this camera.  cv.WaitKey must be called at some point for the
             image to be displayed.
 
-        record - If True, all images captured will be recorded.  If the index
-            given is a filename, this is overwritten to false.
+        record - If True, all images captured will be recorded.  If the
+            identifier given is a filename, this is overwritten to false.
 
         '''
 
-        self.index = index
+        self.identifier = identifier
         self.display = display
         self.record = record
+        self.image = None # Stored image, if identifier is an image filename
         self.capture = None # Underlying opencv capture object
 
         if display:
-            cv.NamedWindow("Camera %s" % index)
+            cv.NamedWindow("Camera %s" % identifier)
 
     def get_frame(self):
         '''Gets a frame from the camera.'''
 
+        if not self.capture and not self.image:
+            self.open_capture()
+
+        if self.image:
+            return cv.CloneImage(self.image)
+
         if self.record:
             pass #TODO
 
-        if not self.capture:
-            self.open_capture()
-
         frame = cv.QueryFrame(self.capture)
         if self.display:
-            cv.ShowImage("Camera %s" % self.index, frame)
+            cv.ShowImage("Camera %s" % self.identifier, frame)
 
         if not frame:
-            raise self.CaptureError('Could not capture frame from index "%s"'
-                % self.index)
+
+            # See if file is an image, not video
+            self.image = cv.LoadImage(self.identifier)
+            if self.image: return cv.CloneImage(self.image)
+
+            raise self.CaptureError('Could not capture frame from identifier '
+                '"%s"' % self.identifier)
 
         return frame
 
@@ -57,21 +71,20 @@ class Camera(object):
 
         '''
         try:
-            self.index = int(self.index)
+            self.identifier = int(self.identifier)
         except ValueError:
             pass
-        if isinstance(self.index, basestring):
-            self.capture = cv.CaptureFromFile(self.index)
+        if isinstance(self.identifier, basestring):
+            self.capture = cv.CaptureFromFile(self.identifier)
             self.record = False
         else:
-            self.capture = cv.CaptureFromCAM(self.index)
-        return self.capture
+            self.capture = cv.CaptureFromCAM(self.identifier)
 
     def close(self):
         '''Close the camera.
 
         The camera can be reopened by getting another frame.
-        
+
         '''
         if self.capture:
             del self.capture
@@ -84,4 +97,4 @@ class Camera(object):
         pass
 
     def __repr__(self):
-        return "<Camera index=%s>" % self.index
+        return "<Camera identifier=%s>" % self.identifier
