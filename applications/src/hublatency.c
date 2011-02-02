e286099c39b66159102c46e062511a1982a64d91
Chris Thunes <cmthunes@ncsu.edu>
2011-02-01 21:05:53 -0500

    Update hub latency to have help and report timings gooder

--- applications/src/hublatency.c
+++ applications/src/hublatency.c
@@ -1,19 +1,68 @@
 
 #include "seawolf.h"
 
-int main(void) {
+#define PRINT_RATE 50.0
+#define PING_RATE 10000.0
+
+static void ping(int n, int count) {
     Seawolf_loadConfig("../conf/seawolf.conf");
-    Seawolf_init("Hub Latency");
+    Seawolf_init(Util_format("Pinger %d", n));
     
     Timer* timer = Timer_new();
+    static char action[16], data[16];
+    char* num = strdup(Util_format("%d", n));
+    int i = n;
 
-    Notify_filter(FILTER_MATCH, "PING PING");
-    
+    Notify_filter(FILTER_MATCH, Util_format("PING %d", n));
+    Util_usleep(1.0);
+
+    const int rate = count * (PING_RATE / PRINT_RATE);
+
     while(true) {
-        Notify_send("PING", "PING");
-        Notify_get(NULL, NULL);
-        printf("Ping: %.4f\n", Timer_getDelta(timer));
+        Notify_send("PING", Util_format("%d", n));
+        Notify_get(action, data);
+
+        assert(strcmp(action, "PING") == 0);
+        assert(strcmp(data, num) == 0);
+
+        if(i == (rate / count) * n) {
+            printf("%3d: %.4f\n", n, (Timer_getDelta(timer) / rate) - (1 / PING_RATE));
+        }
+
+        i = (i + 1) % rate;
+        Util_usleep(1 / PING_RATE);
     }
+}
+
+static void usage(const char* pname) {
+    printf("usage: %s [-h] [-c <count>]\n", pname);
+    exit(1);
+}
+
+int main(int argc, char** argv) {
+    int count = 1;
+
+    /* Parse options */
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-h") == 0) {
+            usage(argv[0]);
+        } else if((argc - i) > 1) {
+            if(strcmp(argv[i], "-c") == 0) {
+                count = atoi(argv[++i]);
+            } else {
+                usage(argv[0]);
+            }
+        } else {
+            usage(argv[0]);
+        }
+    }
+
+    for(int i = 1; i < count; i++) {
+        if(fork() == 0) {
+            ping(i, count);
+        }
+    }
+    ping(0, count);
 
     return 0;
 }
