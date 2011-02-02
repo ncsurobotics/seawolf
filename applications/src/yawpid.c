67160732b6adf04c78521c9460b1cddd302d94ef
Chris Thunes <cmthunes@ncsu.edu>
2011-02-02 11:13:53 -0500

    Yaw PID fix

--- applications/src/yawpid.c
+++ applications/src/yawpid.c
@@ -2,9 +2,25 @@
 #include "seawolf.h"
 #include "seawolf3.h"
 
+#include <math.h>
+
 static void dataOut(double mv) {
     float out = Util_inRange(-1.0, mv, 1.0);
     Notify_send("THRUSTER_REQUEST", Util_format("Yaw %.4f", out));
+}
+
+static double angleError(double a1, double a2) {
+    double error = (a2 - a1);
+
+    if(fabs(error) > 180) {
+        if(error < 0) {
+            return (360.0 - fabs(error));
+        }
+        
+        return -(360.0 - fabs(error));
+    }
+
+    return error;
 }
 
 int main(void) {
@@ -15,16 +31,16 @@ int main(void) {
     char data[64];
     double mv;
     bool paused = (Var_get("YawPID.Paused") != 0.0);
+    float heading = Var_get("YawPID.Heading");
 
     Notify_filter(FILTER_MATCH, "UPDATED YawPID.Coefficients");
     Notify_filter(FILTER_MATCH, "UPDATED YawPID.Heading");
     Notify_filter(FILTER_MATCH, "UPDATED YawPID.Paused");
     Notify_filter(FILTER_MATCH, "UPDATED IMU");
 
-    pid = PID_new(Var_get("YawPID.Heading"),
-                  Var_get("YawPID.p"),
-                  Var_get("YawPID.i"),
-                  Var_get("YawPID.d"));
+    pid = PID_new(0.0, Var_get("YawPID.p"),
+                       Var_get("YawPID.i"),
+                       Var_get("YawPID.d"));
     dataOut(0.0);
 
     while(true) {
@@ -38,8 +54,8 @@ int main(void) {
                                 Var_get("YawPID.d"));
             PID_resetIntegral(pid);
         } else if(strcmp(data, "YawPID.Heading") == 0) {
-            PID_setSetPoint(pid, Var_get("YawPID.Heading"));
-            mv = PID_update(pid, yaw);
+            heading = Var_get("YawPID.Heading");
+            mv = PID_update(pid, angleError(heading, yaw));
             if(paused) {
                 Var_set("YawPID.Paused", 0.0);
             }
@@ -55,7 +71,7 @@ int main(void) {
                 Notify_send("PIDPAUSED", "Yaw");
             }
         } else if(strcmp(data, "IMU") == 0 && paused == false) {
-            mv = PID_update(pid, yaw);
+            mv = PID_update(pid, angleError(heading, yaw));
         }
         
         if(paused == false) {
