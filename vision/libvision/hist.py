39214cfe53878f2bd361d4f115c509ed94958300
Michael Brown <msbrown3@ncsu.edu>
2011-05-31 23:12:40 -0400

    Histogram statistics tools.  This should have been committed earlier for the Traker to work.

--- vision/libvision/hist.py
+++ vision/libvision/hist.py
@@ -1,5 +1,6 @@
 
 from __future__ import division
+from math import sqrt
 
 import cv
 
@@ -16,3 +17,37 @@ def histogram_image(hist, color=(255,255,255), background_color=(0,0,0), num_bin
         cv.Line(img, (i,num_bins), (i,num_bins-height), color)
 
     return img
+
+def calc_expected_value(hist, num_bins=256):
+    sum = 0
+    num_datapoints = 0
+    for i in xrange(num_bins):
+        value = cv.QueryHistValue_1D(hist, i)
+        num_datapoints += value
+        sum += value*i
+    return sum / num_datapoints
+
+def calc_variance(hist, expected_value=None, num_bins=256):
+
+    if expected_value is None:
+        expected_value = calc_expected_value(hist, num_bins)
+
+    variance = 0
+    sum = 0
+    for i in xrange(num_bins):
+        value = cv.QueryHistValue_1D(hist, i)
+        variance += value * (i-expected_value)**2
+        sum += value
+    return variance / sum
+
+def calc_stddev(hist, expected_value=None, num_bins=256):
+    return sqrt(
+        calc_variance(hist, expected_value, num_bins=256)
+    )
+
+def num_stddev_from_mean(hist, x, num_bins=256):
+    '''Calculates how many standard deviations x is away from the mean.'''
+
+    expected_value = calc_expected_value(hist, num_bins)
+    stddev = calc_stddev(hist, expected_value, num_bins)
+    return (x - expected_value) / stddev
