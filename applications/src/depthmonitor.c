bc5b3e5af1fedb032d35702d1a075b90696aa9d2
Chris Thunes <cmthunes@ncsu.edu>
2011-06-25 12:45:49 -0400

    New subscription based depth monitor

--- applications/src/depthmonitor.c
+++ applications/src/depthmonitor.c
@@ -14,13 +14,14 @@
 
 #define ADJUST(x) Util_inRange(MIN_SPEED, (x), MAX_SPEED)
 
-static float depth_heading = 0;
 static bool quit = false;
 
-static void* control_heading(void* _n) {
-    int c;
+static int control_heading(void) {
+    float depth_heading = 0;
     WINDOW* w = newwin(1, 1, 0, 0);
-    Var_set("DepthHeading", depth_heading);
+    int c;
+
+    Var_set("DepthPID.Heading", depth_heading);
     while(true) {
         c = wgetch(w);
         if(c == 'q') {
@@ -32,83 +33,67 @@ static void* control_heading(void* _n) {
         } else {
             continue;
         }
-        Var_set("DepthHeading", depth_heading);
+        Var_set("DepthPID.Heading", depth_heading);
     }
 
     quit = true;
-    Var_set("DepthHeading", 0);
+    Var_set("DepthPID.Heading", 0);
     
-    return NULL;
+    return 0;
 }
 
 int main(void) {
+    float stern, port, star, bow, strafe;
+    float depth, depthheading, seapitch, searoll, seayaw;
+
+    char* display = "  \n\
+        Seawolf III    \n\
+                       \n\
+   %10.2f/%.2f \tDepth Heading/Depth  \n\
+                       \n\
+   %10.2f %.2f \tPitch/Roll \n\
+      %10.2f   \tYaw   \n\
+                       \n\
+                       \n\
+         %7.2f         \n\
+                       \n\
+         %7.2f         \n\
+  %7.2f       %7.2f      \n\
+                       \n\
+                       \n\
+         %7.2f         \n\
+";
+    
+    /* Init libseawolf */
     Seawolf_loadConfig("../conf/seawolf.conf");
     Seawolf_init("Remote control");
 
+    /* Init curses */
     initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
 
-    char* display = "    \n\
-     Seawolf III         \n\
-                         \n\
-  %10.2f/%.2f            \n\
-  %10.2f %.2f Pitch/Roll \n\
-     %10.2f Yaw          \n\
-                         \n\
-                         \n\
-        %-6d %4d         \n\
-                         \n\
-                         \n\
-    %-7d     %7d         \n\
-                         \n\
-                         \n\
-                         \n\
-    %10d                 \n\
-";
+    Task_background(control_heading);
 
-    int aft = 0, portx = 0, porty = 0, starx = 0, stary = 0;
-    float depth=0, seapitch = 0, searoll = 0, seayaw = 0;
-    char data[64];
-
-    pthread_t control_thread;
-    pthread_create(&control_thread, NULL, control_heading, NULL);
-
-    /* Filter for our messages */
-    Notify_filter(FILTER_MATCH, "UPDATED Aft");
-    Notify_filter(FILTER_MATCH, "UPDATED PortY");
-    Notify_filter(FILTER_MATCH, "UPDATED StarY");
-    Notify_filter(FILTER_MATCH, "UPDATED PortX");
-    Notify_filter(FILTER_MATCH, "UPDATED StarX");
-    Notify_filter(FILTER_MATCH, "UPDATED Depth");
-    Notify_filter(FILTER_MATCH, "UPDATED IMU");
-    Notify_filter(FILTER_MATCH, "UPDATED DepthHeading");
-
-    while(! quit) {
+    Var_bind("Strafe", &strafe);
+    Var_bind("Stern", &stern);
+    Var_bind("Bow", &bow);
+    Var_bind("Port", &port);
+    Var_bind("Star", &star);
+    Var_bind("Depth", &depth);
+    Var_bind("SEA.Pitch", &seapitch);
+    Var_bind("SEA.Roll", &searoll);
+    Var_bind("SEA.Yaw", &seayaw);
+    Var_bind("Depth", &depth);
+    Var_bind("DepthPID.Heading", &depthheading);
+    
+    while(quit == false) {
         clear();
-        printw(display, depth_heading, depth, seapitch, searoll, seayaw, porty, stary, portx, starx, aft);
+        printw(display, depthheading, depth, seapitch, searoll, seayaw, bow, strafe, port, star, stern);
         refresh();
 
-        Notify_get(NULL, data);
-
-        if(strcmp(data, "Aft") == 0) {
-            aft = (int) Var_get("Aft");
-        } else if(strcmp(data, "PortY") == 0) {
-            porty = (int) Var_get("PortY");
-        } else if(strcmp(data, "StarY") == 0) {
-            stary = (int) Var_get("StarY");
-        } else if(strcmp(data, "PortX") == 0) {
-            portx = (int) Var_get("PortX");
-        } else if(strcmp(data, "StarX") == 0) {
-            starx = (int) Var_get("StarX");
-        } else if(strcmp(data, "Depth") == 0) {
-            depth = Var_get("Depth");
-        } else if(strcmp(data, "IMU") == 0) {
-            seapitch = Var_get("SEA.Pitch");
-            searoll = Var_get("SEA.Roll");
-            seayaw = Var_get("SEA.Yaw");
-        }
+        Var_sync();
     }
 
     endwin();
