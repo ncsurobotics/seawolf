b6fa28fc072a7c383b2ce168558dd2dccd064885
Jeffrey Fowler <jrfowle2@ncsu.edu>
2011-07-15 04:21:38 -0400

    Heavily modified buoy entity to use feature detection

--- vision/entities/buoys.py
+++ vision/entities/buoys.py
@@ -3,6 +3,7 @@ from __future__ import division
 import math
 import ctypes
 from collections import namedtuple
+from itertools import combinations
 
 import cv
 
@@ -15,6 +16,9 @@ BUOY_GREEN = 0
 BUOY_RED = 1
 BUOY_YELLOW = 2
 
+INIT_SEARCH_LOST_TIMEOUT = 5  # Frames we stop looking for features after we don't see any features
+INIT_TRACKING_THRESHOLD = 3  # Frames to see buoys before tracking
+
 ############################### Tuning Values ###############################
 #FILTER_TYPE = cv.CV_GAUSSIAN
 FILTER_TYPE = cv.CV_GAUSSIAN
@@ -25,8 +29,11 @@ FILTER_SIZE = 11
 MIN_BLOB_SIZE = 100
 TRACKING_MIN_Z_SCORE = 10
 TRACKING_ALPHA = 0.6
-TRACKING_TEMPLATE_MULTIPLIER = 2
-TRACKING_SEARCH_AREA_MULTIPLIER = 8
+TRACKING_TEMPLATE_MULTIPLIER = 0.25
+TRACKING_SEARCH_AREA_MULTIPLIER = 0.6
+
+MAX_Y_SEPARATION = 20
+CENTER_ERROR_PERCENT = 0.2
 
 class BuoysEntity(VisionEntity):
 
@@ -38,6 +45,8 @@ class BuoysEntity(VisionEntity):
         self.state = "searching"
         self.trackers = []
         self.buoy_locations = []
+        self.seen_buoy_count = 0
+        self.initial_search_lost = -1
 
         self.saturation_adaptive_thresh_blocksize = 51
         self.saturation_adaptive_thresh = 15
@@ -53,11 +62,14 @@ class BuoysEntity(VisionEntity):
 
     def find(self, frame, debug=True):
 
+        '''
         # Scale image to reduce processing
         frame_scaled = cv.CreateImage((frame.width*0.5, frame.height*0.5), 8, 3)
         cv.Resize(frame, frame_scaled)
         cv.SetImageROI(frame, (0, 0, frame.width*0.5, frame.height*0.5))
         #scale_in_place(frame, (frame.width*0.5, frame.height*0.5))
+        '''
+        frame_scaled = frame
 
         # debug_frame will be copied to frame at the end if debug=True
         if debug:
@@ -65,26 +77,30 @@ class BuoysEntity(VisionEntity):
         else:
             debug_frame = False
 
-        # Searching State
-        # Search for buoys, then move to tracking when they are found
-        if self.state == "searching":
+        # Initial Searching
+        if self.initial_search_lost < INIT_SEARCH_LOST_TIMEOUT:
 
             trackers = self.initial_search(frame_scaled, debug_frame)
             if trackers:
-                self.trackers = trackers
-                self.state = "tracking"
-                print "Tracking"
 
-                # Finish mission to make sure the first entity returned to
-                # mission control has locations for all buoys
+                self.initial_search_lost = 0
+                self.trackers = trackers
+
+                # Finish mission to make sure the first entity returned to mission
+                # control has locations for all buoys
                 self.buoy_locations = map(lambda x: adjust_location(x.object_center, frame_scaled.width, frame_scaled.height), self.trackers)
                 if debug_frame:
                     cv.Copy(debug_frame, frame)
                 return True
 
-        # Tracking State
+            else:
+                # Increment initial_search_lost, but only if we've seen it enough
+                if self.initial_search_lost != -1:
+                    self.initial_search_lost += 1
+
+        # Tracking
         num_buoys_found = 0
-        if self.state == "tracking":
+        if self.trackers:
             num_buoys_found, locations = self.buoy_track(frame_scaled, self.trackers, debug_frame)
             if num_buoys_found > 0:
                 self.buoy_locations = locations
@@ -98,34 +114,38 @@ class BuoysEntity(VisionEntity):
     def initial_search(self, frame, debug_frame):
         '''Search for buoys, return trackers when at least 2 are found.'''
 
-        blobs, labeled_image = self.find_blobs(frame, debug_frame)
-        buoy_blobs = self.extract_buoys_from_blobs(blobs, labeled_image)
+        features = self.find_features(frame, debug_frame)
+        middle_buoy, buoy_dist = self.extract_buoys_from_features(features)
 
-        trackers = []
-        if buoy_blobs:
+        if middle_buoy:
+            self.seen_buoy_count += 1
+        else:
+            self.seen_buoy_count = 0
+
+        tracker = None
+        if self.seen_buoy_count >= INIT_TRACKING_THRESHOLD:
 
             # Initialize Tracking
-            for blob in buoy_blobs:
-                tracker = libvision.Tracker(
-                    frame,
-                    blob.centroid,
-                    (blob.roi[2]*TRACKING_TEMPLATE_MULTIPLIER,
-                            blob.roi[3]*TRACKING_TEMPLATE_MULTIPLIER),
-                    (blob.roi[2]*TRACKING_SEARCH_AREA_MULTIPLIER,
-                            blob.roi[3]*TRACKING_SEARCH_AREA_MULTIPLIER),
-                    min_z_score=TRACKING_MIN_Z_SCORE,
-                    alpha=TRACKING_ALPHA,
-                    #debug=True,
-                )
-                trackers.append(tracker)
+            tracker = libvision.Tracker(
+                frame,
+                middle_buoy,
+                (buoy_dist*TRACKING_TEMPLATE_MULTIPLIER,
+                        buoy_dist*TRACKING_TEMPLATE_MULTIPLIER),
+                (buoy_dist*TRACKING_SEARCH_AREA_MULTIPLIER,
+                        buoy_dist*TRACKING_SEARCH_AREA_MULTIPLIER),
+                min_z_score=TRACKING_MIN_Z_SCORE,
+                alpha=TRACKING_ALPHA,
+                debug=True,
+            )
 
         # Debug info
-        if debug_frame and blobs:
-            for blob in blobs:
-                cv.Rectangle(debug_frame, (blob.roi[0], blob.roi[1]),
-                         (blob.roi[0]+blob.roi[2], blob.roi[1]+blob.roi[3]), (0,255,0))
+        if debug_frame and middle_buoy:
+            cv.Circle(debug_frame, middle_buoy, buoy_dist*TRACKING_TEMPLATE_MULTIPLIER/2, (0,255,0), 2)
 
-        return trackers
+        if tracker:
+            return [tracker]
+        else:
+            return []
 
     def buoy_track(self, frame, trackers, debug_frame):
         '''Update trackers and return (num_buoys_found, buoy_locations).'''
@@ -152,82 +172,61 @@ class BuoysEntity(VisionEntity):
 
         return num_buoys_found, locations
 
-    def extract_buoys_from_blobs(self, blobs, labeled_image):
-        blobs = filter(self.blob_filter, blobs)
-        if len(blobs) == 2 or len(blobs) == 3:
-            return blobs
+    def extract_buoys_from_features(self, features):
 
-    def blob_filter(self, blob):
-        if blob.size > 700:
-            print "Filtering based on large blob size"
-            return False
+        if len(features) < 3:
+            return None, None
 
-        width = blob.roi[2]
-        height = blob.roi[3]
-        ratio = width / height
-        if ratio < 1:
-            ratio = 1 / ratio
-        if ratio > 2:
-            print "Filtering based on aspect ratio"
-            return False
+        for tripplet in combinations(features, 3):
 
-        return True
+            # Sort features by x value
+            feature1,feature2,feature3 = sorted(tripplet, key=lambda x:x[0])
 
-    def find_blobs(self, frame, debug_image):
-        '''Find blobs in an image.
+            # Test that center feature is in middle of outside features
+            center = (feature1[0] + feature3[0]) / 2
+            if abs(center - feature2[0]) > CENTER_ERROR_PERCENT * (feature3[0]-feature1[0]):
+                continue
 
-        Hopefully this gets blobs that correspond with
-        buoys, but any intelligent checking is done outside of this function.
+            # Go through every pair
+            tripplet_is_valid = True
+            for a,b in combinations((feature1,feature2,feature3), 2):
 
-        '''
+                # Ignore tripplets with pairs with large y separations
+                if abs(a[1] - b[1]) > MAX_Y_SEPARATION:
+                    tripplet_is_valid = False
+                    break
+            if not tripplet_is_valid:
+                continue
+
+            return feature2, feature3[0]-feature1[0]
+
+        return None, None
+
+    def find_features(self, frame, debug_image):
+        '''Find features in an image.'''
 
         # Get Channels
         hsv = cv.CreateImage(cv.GetSize(frame), 8, 3);
         cv.CvtColor(frame, hsv, cv.CV_BGR2HSV)
-        #saturation = libvision.misc.get_channel(hsv, 1)
-        red = libvision.misc.get_channel(frame, 2)
+        grey = libvision.misc.get_channel(hsv, 2)
 
-        # Adaptive Threshold
-        '''
-        cv.AdaptiveThreshold(saturation, saturation,
-            255,
-            cv.CV_ADAPTIVE_THRESH_MEAN_C,
-            cv.CV_THRESH_BINARY_INV,
-            self.saturation_adaptive_thresh_blocksize - self.saturation_adaptive_thresh_blocksize%2 + 1,
-            self.saturation_adaptive_thresh,
-        )
-        '''
-        cv.AdaptiveThreshold(red, red,
-            255,
-            cv.CV_ADAPTIVE_THRESH_MEAN_C,
-            cv.CV_THRESH_BINARY,
-            self.red_adaptive_thresh_blocksize - self.red_adaptive_thresh_blocksize%2 + 1,
-            -1*self.red_adaptive_thresh,
-        )
+        # Feature Detection
+        eigimage = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_32F,1)
+        tmpimage = cv.CreateImage(cv.GetSize(frame), cv.IPL_DEPTH_32F,1)
+        cornercount = 4
+        qualitylevel = .2
+        mindistance = 40
+        blockSize = 7
+        corners = cv.GoodFeaturesToTrack(grey,eigimage,tmpimage,cornercount,qualitylevel,mindistance,None,blockSize,0,0.4)
 
-        kernel = cv.CreateStructuringElementEx(9, 9, 4, 4, cv.CV_SHAPE_ELLIPSE)
-        #cv.Erode(saturation, saturation, kernel, 1)
-        #cv.Dilate(saturation, saturation, kernel, 1)
-        cv.Erode(red, red, kernel, 1)
-        cv.Dilate(red, red, kernel, 1)
-
-        #buoys_filter = cv.CreateImage(cv.GetSize(frame), 8, 1)
-        #cv.And(saturation, red, buoys_filter)
-        buoys_filter = red
-
+        #determine if three corners are in a reasonable orientation 
         if debug_image:
-            #cv.NamedWindow("Saturation")
-            #cv.ShowImage("Saturation", saturation)
-            #cv.NamedWindow("Red")
-            #cv.ShowImage("Red", red)
-            cv.NamedWindow("AdaptiveThreshold")
-            cv.ShowImage("AdaptiveThreshold", buoys_filter)
 
-        # Get blobs
-        labeled_image = cv.CreateImage(cv.GetSize(buoys_filter), 8, 1)
-        blobs = libvision.blob.find_blobs(buoys_filter, labeled_image, MIN_BLOB_SIZE, 10)
+            for corner in corners:
+                corner_color = (0,0,255)
+                cv.Circle(debug_image, corner, 15, corner_color, 2,8,0)
 
-        return blobs, labeled_image
+        return corners
 
     def __repr__(self):
         return "<BuoysEntity buoy_locations=%s>" % self.buoy_locations
