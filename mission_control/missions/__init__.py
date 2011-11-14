9d6fe3fa0ace7ecfa1f0028c13957dd42e699322
Michael Brown <mbrown1413@gmail.com>
2011-11-13 19:27:07 -0500

    Add buoy mission to mission_control/run.py

--- mission_control/missions/__init__.py
+++ mission_control/missions/__init__.py
@@ -4,11 +4,11 @@ from base import MissionBase
 from test import TestMission
 from gate import GateMission
 from path import PathMission
-
+from buoy import BuoyMission
 
 mission_classes = {
     'test': TestMission,
     'gate': GateMission,
     'path': PathMission,
-
+    'buoy': BuoyMission,
 }
