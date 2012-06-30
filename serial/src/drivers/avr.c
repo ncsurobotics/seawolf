cd13765c340fbc761dbb5e9b8a7c4910b2ca1761
Chris Thunes <cmthunes@ncsu.edu>
2012-06-30 18:52:06 -0400

    Add support for the rest of the AVR features

--- serial/src/drivers/avr.c
+++ serial/src/drivers/avr.c
@@ -20,16 +20,41 @@ enum Commands {
     SW_SERVO    = 0x02,
     SW_STATUS   = 0x03,
     SW_DEPTH    = 0x04,
-    SW_TEMP     = 0x05
+    SW_TEMP     = 0x05,
+    SW_SOLENOID = 0x06,
+    SW_BATTERY  = 0x07,
+    SW_KILL     = 0x08
 };
 
-enum Motors {
+typedef enum {
+    SERVO1 = 0,
+    SERVO2 = 1
+} Servo;
+
+typedef enum {
+    SOLENOID1 = 0,
+    SOLENOID2 = 1,
+    SOLENOID3 = 2
+} Solenoid;
+
+typedef enum {
     PORT   = 0,
     STAR   = 1,
     STERN  = 2,
     BOW    = 3,
     STRAFE = 4
-};
+} Motor;
+
+typedef enum {
+    SLA1 = 0,
+    SLA2 = 1,
+    LIPO = 2
+} Battery;
+
+typedef enum {
+    KILLED = 0,
+    NOT_KILLED = 1
+} KillStatus;
 
 static void avr_synchronize(SerialPort sp) {
     int i;
@@ -89,19 +114,102 @@ static void* receive_thread(void* _sp) {
         case SW_TEMP:
             set_temp(frame[1] << 8 | frame[2]);
             break;
+
+        case SW_BATTERY:
+            switch(frame[1]) {
+            case SLA1:
+                Logging_log(WARNING, "SLA battery 1 low!");
+                break;
+
+            case SLA2:
+                Logging_log(WARNING, "SLA battery 2 low!");
+                break;
+
+            case LIPO:
+                Logging_log(WARNING, "LiPo batteries low!");
+                break;
+            }
+
+            Var_set("StatusLight", 3);
+            break;
+
+        case SW_KILL:
+            /* Send appropirate notification */
+            if(frame[2] == 0) {
+                Notify_send("EVENT", "PowerKill");
+            } else {
+                Notify_send("EVENT", "SystemReset");
+            }
+            break;
         }
     }
 
     return NULL;
 }
 
-static void set_thruster(SerialPort sp, enum Motors motor, float value) {
+static void set_thruster(SerialPort sp, Motor motor, float value) {
     char command[3];
+
     command[0] = SW_MOTOR;
     command[1] = motor;
     command[2] = (int) (MOTOR_RANGE * value);
+
     Serial_send(sp, command, 3);
     Util_usleep(0.01); /* XXX: Remove this when the avr works right */
+}
+
+#if 0
+
+static void set_solenoid(SerialPort sp, Solenoid solenoid, float value) {
+    char command[3];
+
+    command[0] = SW_SOLENOID;
+    command[1] = solenoid;
+    command[2] = (int) value;
+
+    Serial_send(sp, command, 3);
+}
+
+static void set_servo(SerialPort sp, Servo servo, float value) {
+    char command[3];
+
+    command[0] = SW_SERVO;
+    command[1] = servo;
+    command[2] = (int) value;
+
+    Serial_send(sp, command, 3);
+}
+
+#endif
+
+static void set_status(SerialPort sp, int value) {
+    unsigned char command[3];
+
+    command[0] = SW_STATUS;
+    command[1] = 0;
+
+    switch(value) {
+    case 0:
+        command[2] = 200;
+        break;
+
+    case 1:
+        command[2] = 100;
+        break;
+
+    case 2:
+        command[2] = 0;
+        break;
+
+    case 3:
+        command[2] = 180;
+        break;
+
+    default:
+        Logging_log(ERROR, "Invalid StatusLight value!");
+    }
+
+    Serial_send(sp, command, 3);
 }
 
 int main(int argc, char** argv) {
@@ -140,6 +248,16 @@ int main(int argc, char** argv) {
     Var_subscribe("Port");
     Var_subscribe("Star");
 
+#if 0
+    Var_subscribe("Solenoid.0");
+    Var_subscribe("Solenoid.1");
+    Var_subscribe("Solenoid.2");
+    Var_subscribe("Servo.0");
+    Var_subscribe("Servo.1");
+#endif
+
+    Var_subscribe("StatusLight");
+
     while(true) {
         Var_sync();
 
@@ -163,6 +281,31 @@ int main(int argc, char** argv) {
             set_thruster(sp, STAR, Var_get("Star"));
         }
 
+#if 0
+        if(Var_poked("Solenoid.0")) {
+            set_solenoid(sp, SOLENOID1, Var_get("Solenoid.0"));
+        }
+
+        if(Var_poked("Solenoid.1")) {
+            set_solenoid(sp, SOLENOID2, Var_get("Solenoid.1"));
+        }
+
+        if(Var_poked("Solenoid.2")) {
+            set_solenoid(sp, SOLENOID3, Var_get("Solenoid.2"));
+        }
+
+        if(Var_poked("Servo.0")) {
+            set_servo(sp, SERVO1, Var_get("Servo.0"));
+        }
+
+        if(Var_poked("Servo.1")) {
+            set_servo(sp, SERVO2, Var_get("Servo.1"));
+        }
+#endif
+
+        if(Var_poked("StatusLight")) {
+            set_status(sp, Var_get("StatusLight"));
+        }
     }
 
     Serial_closePort(sp);
