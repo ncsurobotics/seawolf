5c5b788575196174939fa57cbc615413cef3e8cf
Ryan <edwardsr75@gmail.com>
2014-02-16 12:23:29 -0500

    Increased adaptive threshold and increased number of dilate cycles to better identify black and white bins

--- vision/entities/blobtesting.py
+++ vision/entities/blobtesting.py
@@ -45,9 +45,13 @@ class Bin(object):
         self.corner2 = corner2
         self.corner3 = corner3
         self.corner4 = corner4
+        self.corners = [corner1,corner2,corner3,corner4]
         self.midx = rect_midpointx(corner1,corner2,corner3,corner4)
         self.midy = rect_midpointy(corner1,corner2,corner3,corner4)
         self.area = line_distance(corner1,corner2)*line_distance(corner1,corner4)
+        self.id = 0        # id identifies which bin your looking at
+        self.lastseen = 2 # how recently you have seen this bin
+        self.seencount = 1 # how many times you have seen this bin (if you see it enough it becomes confirmed)
 
 def line_distance(corner_a, corner_b):
     distance = math.sqrt((corner_b[0] - corner_a[0])**2 + 
@@ -66,17 +70,25 @@ def rect_midpointy(corner_a, corner_b, corner_c, corner_d):
 class BlobTesting(VisionEntity):
     def init(self):
 
-        self.adaptive_thresh_blocksize = 21  # 27 before competition
-        self.adaptive_thresh = 5 # 23 before competition #9 at competition #11 after hough line changes
+        self.adaptive_thresh_blocksize = 21  
+        self.adaptive_thresh = 7
         self.mid_sep = 50
         self.min_area = 10000
+        
+        self.recent_id = 1
 
+        self.trans_thresh = 25
+
+        self.candidates = []
+        self.confirmed = []
+
+        self.lastseen_thresh = 0
+        self.seencount_thresh = 2
 
     def process_frame(self, frame):
+        #Creation of frames
         self.debug_frame = cv.CreateImage(cv.GetSize(frame), 8, 3)
-        og_frame = cv.CreateImage(cv.GetSize(frame), 8, 3)
         cv.Copy(frame, self.debug_frame)
-        cv.Copy(self.debug_frame, og_frame)
         self.debug_numpy_frame = cv_to_cv2(self.debug_frame)
 
         #CV2 Transforms        
@@ -86,8 +98,10 @@ class BlobTesting(VisionEntity):
         self.numpy_frame = cv2.cvtColor(self.numpy_frame,cv.CV_BGR2HSV)
         
         [self.frame1,self.frame2,self.frame3] = numpy.dsplit(self.numpy_frame,3)
-        self.numpy_frame = self.frame2
+        self.numpy_frame = self.frame2 #Change the frame number to determine what channel to focus on
         
+
+        '''Temporarily converts the image to a cv frame to do the adaptive threshold '''
         self.temp_cv2frame = cv2_to_cv(self.numpy_frame)
         
         cv.AdaptiveThreshold(self.temp_cv2frame, self.temp_cv2frame,
@@ -100,22 +114,17 @@ class BlobTesting(VisionEntity):
         
         kernel = cv.CreateStructuringElementEx(5, 5, 3, 3, cv.CV_SHAPE_ELLIPSE)
         cv.Erode(self.temp_cv2frame, self.temp_cv2frame, kernel, 1)
-        cv.Dilate(self.temp_cv2frame, self.temp_cv2frame, kernel, 5)
+        cv.Dilate(self.temp_cv2frame, self.temp_cv2frame, kernel, 7)
+        '''Returns the frame to a cv2 image'''
         self.numpy_frame = cv_to_cv2(self.temp_cv2frame)
         
-        #cv2.adaptiveThreshold(self.numpy_frame,self.adaptive_thresh,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,self.adaptive_thresh_blocksize,3,self.numpy_frame)
-        
 
-
-
-
-        
+        '''Begins finding contours'''
         contours, hierarchy = cv2.findContours(self.numpy_frame,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        raw_bins = []
+        self.raw_bins = []
         if len(contours)>1: 
             cnt = contours[0]
             cv2.drawContours(self.numpy_frame,contours,-1,(255,255,255),3)
-            #cv2.drawContours(self.debug_numpy_frame,contours,-1,(0,255,0),3)
             self.masks = []
             pts = []
             for h,cnt in enumerate(contours):
@@ -125,51 +134,101 @@ class BlobTesting(VisionEntity):
                 self.masks.append(mask)
             
                 hull = cv2.convexHull(cnt)
-                #print hull
                 rect = cv2.minAreaRect(cnt)
                 box = cv2.cv.BoxPoints(rect)
                 box = numpy.int0(box)
                 new_bin = Bin(tuple(box[0]),tuple(box[1]),tuple(box[2]),tuple(box[3]))
-                raw_bins.append(new_bin)
+                new_bin.id = self.recent_id
+                self.recent_id = self.recent_id + 1
+                self.raw_bins.append(new_bin)
                 for pt in box:
                     type(tuple(pt))
                     cv2.circle(self.numpy_frame,tuple(pt),5,(255,255,255),-1,8,0)
                     pts.append(pt)
-                #cv2.drawContours(self.numpy_frame,[box],0,(255,255,255),2)
-                for bin1 in raw_bins[:]:
-                    for bin2 in raw_bins[:]: 
-                        if bin1 in raw_bins and bin2 in raw_bins and math.fabs(bin1.midx - bin2.midx) < self.mid_sep and math.fabs(bin1.midy - bin2.midy) < self.mid_sep:
+
+
+
+                '''Removes bins that have centers too close to others (to prevent bins inside bins), and bins that are too small'''
+                for bin1 in self.raw_bins[:]:
+                    for bin2 in self.raw_bins[:]: 
+                        if bin1 in self.raw_bins and bin2 in self.raw_bins and math.fabs(bin1.midx - bin2.midx) < self.mid_sep and math.fabs(bin1.midy - bin2.midy) < self.mid_sep:
                             if bin1.area < bin2.area:
-                                raw_bins.remove(bin1)
+                                self.raw_bins.remove(bin1)
                             elif bin2.area < bin1.area:
-                                raw_bins.remove(bin2)
-                        if bin1 in raw_bins and bin2 in raw_bins:
+                                self.raw_bins.remove(bin2)
+                        if bin1 in self.raw_bins and bin2 in self.raw_bins:
                             if bin1.area < self.min_area:
-                                raw_bins.remove(bin1)
-                            if bin2.area < self.min_area and bin2 in raw_bins:
-                                raw_bins.remove(bin2)
+                                self.raw_bins.remove(bin1)
+                            if bin2.area < self.min_area and bin2 in self.raw_bins:
+                                self.raw_bins.remove(bin2)
         
+        for bin in self.raw_bins:
+            self.match_bins(bin)
+        self.sort_bins()
 
-
-
-
-                
+        
 
  
         self.numpy_to_cv = cv2_to_cv(self.numpy_frame)
-        #print type(self.numpy_to_cv)
         self.debug_final_frame = cv2_to_cv(self.debug_numpy_frame)
-        
-        for bin in raw_bins:
-            print bin.area
-            cv.Circle(self.debug_final_frame, bin.corner1, 5, (0,255,0), -1,8,0)
-            cv.Circle(self.debug_final_frame, bin.corner2, 5, (0,255,0), -1,8,0)
-            cv.Circle(self.debug_final_frame, bin.corner3, 5, (0,255,0), -1,8,0)
-            cv.Circle(self.debug_final_frame, bin.corner4, 5, (0,255,0), -1,8,0)
+        self.draw_bins()
         
 
         svr.debug("CV", self.debug_final_frame)
         svr.debug("CV2", self.numpy_to_cv)
-        #svr.debug("Original", og_frame)
+
+    def match_bins(self,target):
+        found = 0
+        for bin in self.candidates:
+            if math.fabs(bin.midx-target.midx)<self.trans_thresh and math.fabs(bin.midy-target.midy)<self.trans_thresh:
+                print bin.seencount
+                bin.midx = target.midx
+                bin.midy = target.midy
+                bin.corner1 = target.corner1
+                bin.corner2 = target.corner2
+                bin.corner3 = target.corner3
+                bin.corner4 = target.corner4
+                print "still ",bin.seencount
+                bin.seencount += 2
+                print "new seencount ",bin.seencount
+                bin.lastseen += 6
+                found = 1
+        for bin in self.confirmed:
+            if math.fabs(bin.midx-target.midx)<self.trans_thresh and math.fabs(bin.midy-target.midy)<self.trans_thresh:
+                target.id = bin.id
+                bin = target
+                bin.lastseen += 6
+                found = 1
+        if found == 0:
+            self.candidates.append(target)
+            target.lastseen + 3
+
+
+    def sort_bins(self):
+        for bin in self.candidates[:]:
+            print "last seen is ",bin.lastseen
+            print "seencount is ",bin.seencount
+            bin.lastseen -=1
+            if bin.seencount >= self.seencount_thresh:
+                self.confirmed.append(bin)
+                print "confirmed appended"
+            if bin.lastseen < self.lastseen_thresh:
+                self.candidates.remove(bin)
+        for bin in self.confirmed[:]:
+            bin.lastseen -= 1
+            if bin.lastseen < self.lastseen_thresh:
+                self.confirmed.remove(bin)
+                print "confirmed removed"
+
+
+    def draw_bins(self):
+        for bin in self.raw_bins:
+            cv.Circle(self.debug_final_frame, bin.corner1, 5, (0,255,0), -1,8,0)
+            cv.Circle(self.debug_final_frame, bin.corner2, 5, (0,255,0), -1,8,0)
+            cv.Circle(self.debug_final_frame, bin.corner3, 5, (0,255,0), -1,8,0)
+            cv.Circle(self.debug_final_frame, bin.corner4, 5, (0,255,0), -1,8,0)
+            cv.Circle(self.debug_final_frame, (int(bin.midx),int(bin.midy)), 5, (0,255,0), -1,8,0)
+
+
 
 
