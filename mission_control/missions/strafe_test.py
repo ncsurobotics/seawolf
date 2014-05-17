a7d5e4b97dfcce5998b9fb9cde7d6ef6dd7bbc30
mrwiggin <mrwiggin@ncsu.edu>
2014-05-17 14:52:28 -0400

    Added strafe tester

---
+++ mission_control/missions/strafe_test.py
@@ -0,0 +1,43 @@
+# this is the template for mission control, shouldnt be ran
+
+from __future__ import division
+
+from vision import entities
+from missions.base import MissionBase
+from vision import process_manager
+import sw3
+
+
+STRAFE_SPEED = 0.3
+
+
+class MissionTemplate(MissionBase):
+	
+	def __init__(self):
+		self.gate_seen = 0
+		self.gate_lost = 0
+		
+	
+	
+	def init(self):
+		self.process_manager.start_process(entities.GateEntity, "gate", "forward", debug=True)
+		sw3.nav.do(sw3.CompoundRoutine(
+			sw3.Strafe(STRAFE_SPEED),
+			sw3.SetDepth(2),
+			sw3.HoldYaw(),
+			))
+		
+		
+	def step(self):
+		if not vision_data: 
+			return
+		
+		gate_data = vision_data['gate']
+		
+		if not gate_data: 
+			return
+		
+		print gate_data
+		
+		
+		
