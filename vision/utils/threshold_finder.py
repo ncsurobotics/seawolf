59f5bdfca88a17555154e71590db113d99cd79ee
dralley <dralley@ncsu.edu>
2014-08-12 23:17:15 -0400

    fixed bug where selecting channel only worked if video was playing

--- vision/utils/threshold_finder.py
+++ vision/utils/threshold_finder.py
@@ -14,6 +14,9 @@ def main():
     if len(argv) > 1:
         path = argv[1]
 
+    if not path:
+        raise Exception("Must provide a path to get footage from")
+
     filename = os.path.join(base_dir, path)
 
     if not os.path.isfile(filename):
@@ -25,23 +28,34 @@ def main():
 
     vc = cv2.VideoCapture(filename)
 
-
     cv2.createTrackbar('Threshold', 'original', 25, 75, nothing)
     cv2.createTrackbar('Blocksize', 'original', 25, 49, nothing)
     cv2.createTrackbar('Play Video', 'original', 0, 1, nothing)
     cv2.createTrackbar('HSV', 'original', 1, 2, nothing)
 
-    hsv = cv2.getTrackbarPos('HSV', 'original')
-    frame, debug = get_new_frame(vc,hsv)
-    prior = debug.copy()
+    channel = cv2.getTrackbarPos('HSV', 'original')
+    frame = get_new_frame(vc)
+    prior = frame.copy()
 
     while vc.isOpened():
-        hsv = cv2.getTrackbarPos('HSV', 'original')
+        channel = cv2.getTrackbarPos('HSV', 'original')
         if cv2.getTrackbarPos('Play Video', 'original'):
-            frame, debug = get_new_frame(vc,hsv)
-            prior = debug.copy()
-        else: #### For amusement and/or a seizure, comment out this else block and observe the result ####
-            debug = prior.copy()
+            frame = get_new_frame(vc)
+            prior = frame.copy()
+        else:  #### For amusement and/or a seizure, comment out this else block and observe the result ####
+            frame = prior.copy()
+
+        debug = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        # print "Debug is ", debug.shape
+        (h, s, v) = cv2.split(debug)
+        # print "V is: ", v.shape
+        if channel == 0:
+            debug = h
+        elif channel == 1:
+            debug = s
+        else:
+            debug = v
+        # print "worked"
         debug = refresh_frame(debug)
         cv2.imshow('original', frame)
         cv2.imshow('debug', debug)
@@ -53,7 +67,7 @@ def main():
 def refresh_frame(img):
     block_size = cv2.getTrackbarPos('Blocksize', 'original')
     threshold = cv2.getTrackbarPos('Threshold', 'original')
-    hsv = cv2.getTrackbarPos('HSV', 'original')
+    # channel = cv2.getTrackbarPos('HSV', 'original')
 
     if not block_size % 2 == 1:
         block_size += 1
@@ -62,7 +76,7 @@ def refresh_frame(img):
     if block_size <= 1:
         block_size = 3
         cv2.setTrackbarPos('Blocksize', 'original', block_size)
-    print img.shape
+    # print img.shape
     adaptive = cv2.adaptiveThreshold(img, 255,
                                      cv2.ADAPTIVE_THRESH_MEAN_C,
                                      cv2.THRESH_BINARY_INV,
@@ -80,25 +94,14 @@ def refresh_frame(img):
     return adaptive
 
 
-def get_new_frame(vc,hsv):
+def get_new_frame(vc):
     rval, frame = vc.read()
     if not rval:
         print "End of clip"
         cv2.destroyAllWindows()
         vc.release()
         exit()
-    debug = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    print "Debug is ", debug.shape
-    (h, s, v) = cv2.split(debug)
-    print "V is: ", v.shape
-    if hsv == 0:
-        debug = h
-    elif hsv == 1:
-        debug = s
-    else:
-        debug = v
-        print "worked"
-    return frame, debug
+    return frame
 
 
 def nothing(x):
