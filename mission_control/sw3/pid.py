2a4ec379ea699cd021774e00c9fd15aea694922a
dralley <dralley@ncsu.edu>
2014-11-21 13:56:53 -0500

    cleanup pid handlers again

--- mission_control/sw3/pid.py
+++ mission_control/sw3/pid.py
@@ -25,50 +25,52 @@ class PIDInterface(object):
         sw.var.set(self.namespace + ".Paused", 1.0)
 
 
-limits = {
-    "yaw.max": 180.0,
-    "yaw.min": -180.0,
-    "roll.max": 180.0,
-    "roll.min": -180.0,
-    "pitch.max": 180.0,
-    "pitch.min": -180.0,
-    "depth.max": 20.0,
-    "depth.min": 0.0,
-}
-
-
 def set_yaw(value):
-    if limits['yaw.min'] <= value <= limits['yaw.max']:
+    min = -180
+    max = 180
+
+    if min <= value <= max:
         sw.var.set("YawPID.Heading", value)
     else:
-        raise ValueError("Value for yaw heading must be in range {low} to {high}".format(
-            low=limits['yaw.min'], high=limits['yaw.max'])
+        raise ValueError(
+            "Value for yaw heading must be in range {low} to {high}".format(low=min, high=max)
         )
 
 
 def set_pitch(value):
-    if limits['pitch.min'] <= value <= limits['pitch.max']:
+    min = -25
+    max = 25
+
+    if min <= value <= max:
         sw.var.set("PitchPID.Heading", value)
     else:
-        raise ValueError("Value for pitch heading must be in range {low} to {high}".format(
-            low=limits['pitch.min'], high=limits['pitch.max'])
+        raise ValueError(
+            "Value for pitch heading must be in range {low} to {high}".format(low=min, high=max)
         )
 
 
 def set_roll(value):
-    if limits['roll.min'] <= value <= limits['roll.max']:
+    min = -180
+    max = 180
+
+    if min <= value <= max:
         sw.var.set("RollPID.Heading", value)
     else:
-        raise ValueError("Value for roll heading must be in range {low} to {high}".format(
-            low=limits['roll.min'], high=limits['roll.max'])
+        raise ValueError(
+            "Value for roll heading must be in range {low} to {high}".format(low=min, high=max)
         )
 
 
 def set_depth(value):
-    if limits['depth.min'] <= value <= limits['depth.max']:
+    min = 0
+    max = 20
+
+    if min <= value <= max:
         sw.var.set("DepthPID.Heading", value)
     else:
-        raise ValueError("Cowardly refusing to dive so far! (%.4f)" % (value,))
+        raise ValueError(
+            "Value for depth heading must be in range {low} to {high}".format(low=min, high=max)
+        )
 
 yaw = PIDInterface("YawPID", set_yaw)
 pitch = PIDInterface("PitchPID", set_pitch)
