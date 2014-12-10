e609e0dfccb7d0676e0aa010ff522e3e4834012b
dralley <dralley@ncsu.edu>
2014-12-10 01:18:25 -0500

    moved tmuxp files to conf, added testing automation for thrusters

---
+++ tests/test_sensors.py
@@ -0,0 +1,14 @@
+from __future__ import division, print_function
+
+import time
+import sys
+import seawolf
+
+seawolf.loadConfig("../conf/seawolf.conf")
+seawolf.init("Thruster Test")
+
+"""
+Do stuff here
+"""
+
+seawolf.close()
---
+++ tests/test_thrusters.py
@@ -0,0 +1,38 @@
+from __future__ import division, print_function
+
+import time
+import sys
+import seawolf
+
+seawolf.loadConfig("../conf/seawolf.conf")
+seawolf.init("Thruster Test")
+
+# Names of all the thrusters to be tested
+THRUSTERS = ["Bow", "Stern", "Port", "Star", "StrafeB", "StrafeT"]
+
+# Produce a range of values from -.3 to .3 in increments of .1
+VALUE_RANGE = [x / 10 for x in range(-3, 4, 1)]
+
+
+def test_thruster(name):
+
+    print("Testing {}".format(name))
+    print("----------------")
+    print("Setting {} to ".format(name), end="")
+
+    for val in VALUE_RANGE:
+        seawolf.var.set(name, val)
+
+        print("{}".format(val), end="  ")
+        sys.stdout.flush()
+
+        time.sleep(.7)
+
+    print()
+
+
+for thruster in THRUSTERS:
+    test_thruster(thruster)
+    print()
+
+seawolf.close()
