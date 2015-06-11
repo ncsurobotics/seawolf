c9e8937997f676b062b90c120e7e31bb9e567558
seawolf <ncsurobotics@ncsurobotics.com>
2015-06-11 19:22:17 -0400

    changes from pooltest - swapped sign on mixer to account for miswiring a thruster

--- start-seawolf.sh
+++ start-seawolf.sh
@@ -5,14 +5,14 @@ sleep 1 #TODO: less hackish solution than sleeping
 # hub
 screen -dr seawolf -p 0 -X title hub
 sleep 0.5
-screen -dr seawolf -p hub -X stuff "cd libseawolf
+screen -dr seawolf -p hub -X stuff "cd db
 "
-screen -dr seawolf -p hub -X stuff "./hub
+screen -dr seawolf -p hub -X stuff "seawolf-hub -c ../conf/hub.conf
 "
 
 # serialapp
 screen -dr seawolf -X screen -t serialapp
-sleep 0.5
+sleep 1.5
 screen -dr seawolf -p serialapp -X stuff "cd serial
 "
 screen -dr seawolf -p serialapp -X stuff "./bin/serialapp
@@ -20,14 +20,14 @@ screen -dr seawolf -p serialapp -X stuff "./bin/serialapp
 
 # suite.sh
 screen -dr seawolf -X screen -t suite
-sleep 0.5
+sleep 1.5
 screen -dr seawolf -p suite -X stuff "cd applications
 "
 screen -dr seawolf -p suite -X stuff "./suite.sh
 "
 
 screen -dr seawolf -X screen -t bash
-sleep 0.5
+sleep 1.5
 screen -dr seawolf -p bash -X stuff "cd applications
 "
 screen -dr seawolf -p bash -X stuff "
