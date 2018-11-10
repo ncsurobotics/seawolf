2e22a29850d0be79d00fd7f7651745f22b783f1c
Ben <bjfisher@ncsu.edu>
2018-11-10 10:31:15 -0500

    Added in option to scale and use webcam feed to test vision.

--- vision/test.py
+++ vision/test.py
@@ -26,8 +26,8 @@ def writeOutput(outputFile, count, time, header, output):
   
 def main():
   
-  if len(sys.argv) != 4:
-    error('usage: Entity inputFile outPutfile ')
+  if len(sys.argv) != 4 and len(sys.argv) != 5:
+    error('usage: Entity inputFile outPutfile [scale]\n(for webcam, set inputfile to cam) ')
   if (sys.argv[1] not in Entities.VisEntities):
     errMsg = "Invalied Entity\nPosiibleEntiies are:\n"
     for key in Entities.VisEntities:
@@ -45,15 +45,30 @@ def main():
   header = vision.keys
   writeHeader(outputFile, header)
   count = 0
+  #if inputFile is cam, then we are streaming from the webcam
+  #calling cv2.VideoCapture(0) opens up webcam
+  if inputFile == "cam":
+    inputFile = 0
   cap = cv2.VideoCapture(inputFile)
   if (not cap.isOpened()):
     error("cannot open file %s" % inputFile)
+  scaled = False
+  if len(sys.argv) == 5:
+    scaled = True
+    scale = float(sys.argv[4])
+  else:
+    scale = 1
+  a, frame = cap.read()
+  height, width, _ = frame.shape
+  height = int(scale * height)
+  width = int(scale * width)
   while(1):
       if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
     
       a, frame = cap.read()
+      frame = cv2.resize(frame, (width, height))
       if not a:
         break
       cv2.imshow("original", frame)
