87604e42daafd13ce272a37de6d58147d8aa7826
Ben <bjfisher@ncsu.edu>
2019-01-30 19:01:57 -0500

    Added in some functions for transforming images. Increased simulator-start speed.

--- simulator-start.sh
+++ simulator-start.sh
@@ -1,33 +1,28 @@
 # Create the screen
-screen -d -m -S seawolf
-sleep 1 #TODO: less hackish solution than sleeping
+screen -d -m -S seawolf;
 
 # hub
-screen -dr seawolf -p 0 -X title hub
-sleep 0.5
+screen -dr seawolf -p 0 -X title hub;
 screen -dr seawolf -p hub -X stuff "cd db
 "
 screen -dr seawolf -p hub -X stuff "seawolf-hub -c ../conf/hub.conf
 "
 
 #screen for the srv server
-screen -dr seawolf -X screen -t srv_server
-sleep 1.5
+screen -dr seawolf -X screen -t srv_server;
 screen -dr seawolf -p srv_server -X stuff "python start/srv/server.py
 "
 
 
 #suite.sh
-screen -dr seawolf -X screen -t suite
-sleep 1.5
+screen -dr seawolf -X screen -t suite;
 screen -dr seawolf -p suite -X stuff "cd applications
 "
 screen -dr seawolf -p suite -X stuff "./bin/conductor
 "
 
 #watch variable
-screen -dr seawolf -X screen -t watchvars
-sleep 0.2
+screen -dr seawolf -X screen -t watchvars;
 screen -dr seawolf -p watchvars -X stuff "cd applications
 "
 screen -dr seawolf -p watchvars -X stuff "./bin/watchvariables Port Star Stern Bow StrafeT StrafeB Depth DepthPID.Heading
@@ -36,8 +31,7 @@ screen -dr seawolf -p watchvars -X stuff "
 "
 
 # monitor
-screen -dr seawolf -X screen -t HUD
-sleep 0.2
+screen -dr seawolf -X screen -t HUD;
 screen -dr seawolf -p HUD -X stuff "cd applications
 "
 screen -dr seawolf -p HUD -X stuff "./bin/depthmonitor2
@@ -50,8 +44,7 @@ screen -dr seawolf -X screen -t srv_watch
 
 
 # bash
-screen -dr seawolf -X screen -t bash
-sleep 1.5
+screen -dr seawolf -X screen -t bash;
 screen -dr seawolf -p bash -X stuff "cd applications
 "
 screen -dr seawolf -p bash -X stuff "
@@ -66,22 +59,19 @@ screen -dr seawolf -p bash -X stuff "
 
 #simulator
 screen -dr seawolf -X screen -t simulator
-sleep 1.5
 screen -dr seawolf -p simulator -X stuff "cd simulator
 "
 screen -dr seawolf -p simulator -X stuff "python2 sim.py Conf/test.conf
 "
 
 #gui
-screen -dr seawolf -X screen -t gui
-sleep 1.5
+screen -dr seawolf -X screen -t gui;
 screen -dr seawolf -p gui -X stuff "cd applications/gui/
 "
 screen -dr seawolf -p gui -X stuff "python2 gui.py
 "
 
 # start srv watch command
-sleep 1.5
 screen -dr seawolf -p srv_watch -X stuff "python2 start/srv/watch.py
 "
 
---
+++ vision/Trainer/Plotter/transformer.py
@@ -0,0 +1,72 @@
+import cv2
+import numpy as np
+
+"""
+Params:
+- image
+- box in form x,y,w,h
+- amount to scale by (floating point)
+- offset of output image in output image
+
+Returns: (img, new_rects)
+- scaled image, fit inside of the original dimensions
+- how the rectangles map onto the scaled image
+"""
+
+def scale(img, rect, scale):
+  new_rect = None
+  img = cv2.resize(img, None, fx=scale, fy=scale)
+  new_rect = []
+  for i in range(len(rect)):
+    new_rect.append(scale * rect[i])
+  x,y,w,h = new_rect
+  x,y,w,h = int(x), int(y), int(w), int(h)
+  out_img = np.zeros((h, w, 3), np.uint8)
+  out_img = img[y:y+h, x:x+w]
+
+  
+
+  return out_img, (0,0,w,h)
+
+"""
+Using algorithm from https://www.pyimagesearch.com/2017/01/02/rotate-images-correctly-with-opencv-and-python/
+
+Params:
+
+- image
+- box around object in form x,y,w,h
+- amount to rotate by (degrees)
+- dim of output image (h,w)
+
+Returns: (img, new_rects)
+- rotated image (adjusted size for rotation)
+- how the rectangles map onto the rotated image
+"""
+def rotate(img, rect, angle):
+  h, w, _ = img.shape
+  M = cv2.getRotationMatrix2D((w/2,h/2), -angle,1)
+  cos = np.abs(M[0,0])
+  sin = np.abs(M[0,1])
+  nW = int(h*sin + w*cos)
+  nH = int(h*cos + w*sin)
+  M[0, 2] += (nW/2) - (w/2)
+  M[1, 2] += (nH/2) - (h/2)
+  
+  dst = cv2.warpAffine(img,M,(nW,nH))
+  new_rect = None
+  return dst, new_rect
+
+
+def main():
+  name = './data-1/frame-1.jpg'
+  img = cv2.imread(name)
+  img, _ = scale(img, (100, 100, 400, 400), .3)
+  deg = 0
+  while True:
+    rot_img, _ = rotate(img, None, deg)
+    deg += 10
+    cv2.imshow('image', rot_img)
+    if cv2.waitKey(0) & 0xff == ord('q'):
+      break
+  cv2.destroyAllWindows()
+main()
