ca5d29f6f86e562e13bdecceea3aaf63caac8fa9
Ben <bjfisher@ncsu.edu>
2019-04-22 19:44:30 -0400

    Reverted to proper start-seawolf

--- start-autonomous-seawolf.sh
+++ start-autonomous-seawolf.sh
@@ -1,17 +1,86 @@
 # Create the screen
 screen -d -m -S seawolf
-screen -dr seawolf -p 0 -X title hub
-screen -dr seawolf -X screen -t procs
-screen -dr seawolf -X screen -t serialapp
-screen -dr seawolf -X screen -t suite
-screen -dr seawolf -X screen -t watchvars
-screen -dr seawolf -X screen -t HUD
-screen -dr seawolf -X screen -t imu_cal
-screen -dr seawolf -X screen -t bash
-screen -dr seawolf -X screen -t MISSION
-screen -dr seawolf -X screen -t pneumatics
-screen -dr seawolf -X screen -t cams
+sleep 1 #TODO: less hackish solution than sleeping
 
-# start seawolf processes
-screen -dr seawolf -p procs -X stuff "./procs.sh
+# hub
+screen -dr seawolf -p 0 -X title hub
+sleep 0.5
+screen -dr seawolf -p hub -X stuff "cd db
 "
+screen -dr seawolf -p hub -X stuff "seawolf-hub -c ../conf/hub.conf
+"
+
+# serialapp
+screen -dr seawolf -X screen -t serialapp
+sleep 1.5
+screen -dr seawolf -p serialapp -X stuff "cd serial
+"
+screen -dr seawolf -p serialapp -X stuff "./bin/serialapp
+"
+
+# suite.sh
+screen -dr seawolf -X screen -t suite
+sleep 1.5
+screen -dr seawolf -p suite -X stuff "cd applications
+"
+screen -dr seawolf -p suite -X stuff "./bin/conductor
+"
+
+#watch variable
+screen -dr seawolf -X screen -t watchvars
+sleep 0.2
+screen -dr seawolf -p watchvars -X stuff "cd applications
+"
+screen -dr seawolf -p watchvars -X stuff "./bin/watchvariables Port Star Stern Bow StrafeT StrafeB Depth DepthPID.Heading
+"
+screen -dr seawolf -p watchvars -X stuff "
+"
+
+# monitor
+screen -dr seawolf -X screen -t HUD
+sleep 0.2
+screen -dr seawolf -p HUD -X stuff "cd applications
+"
+screen -dr seawolf -p HUD -X stuff "./bin/depthmonitor2
+"
+screen -dr seawolf -p HUD -X stuff "
+"
+
+# Arduino
+screen -dr seawolf -X screen -t imu_cal
+sleep 0.2
+screen -dr seawolf -p imu_cal -X "cd ~/software/external/razor-9dof-ahrs/Arduino/Razor_AHRS/
+"
+screen -dr seawolf -p imu_cal -X stuff "pwd
+"
+#screen -dr seawolf -p imu_cal -X stuff "arduino
+#"
+
+# bash
+screen -dr seawolf -X screen -t bash
+sleep 1.5
+screen -dr seawolf -p bash -X stuff "cd applications
+"
+screen -dr seawolf -p bash -X stuff "
+"
+
+# mission 
+screen -dr seawolf -X screen -t MISSION
+screen -dr seawolf -p bash -X stuff "cd mission_control
+"
+screen -dr seawolf -p bash -X stuff "
+"
+
+# cameras
+screen -dr seawolf -X screen -t cams
+sleep 0.2
+screen -dr seawolf -p bash -X stuff "sh cam_scripts/start-cams.sh
+"
+screen -dr seawolf -p bash -X stuff "
+"
+screen -dr seawolf -p bash -X stuff "cd 
+"
+
+
+screen -x
+#screen -dr seawolf -X windowlist
--- start-seawolf.sh
+++ start-seawolf.sh
@@ -23,64 +23,15 @@ screen -dr seawolf -X screen -t suite
 sleep 1.5
 screen -dr seawolf -p suite -X stuff "cd applications
 "
-screen -dr seawolf -p suite -X stuff "./bin/conductor
+screen -dr seawolf -p suite -X stuff "./suite.sh
 "
 
-#watch variable
-screen -dr seawolf -X screen -t watchvars
-sleep 0.2
-screen -dr seawolf -p watchvars -X stuff "cd applications
-"
-screen -dr seawolf -p watchvars -X stuff "./bin/watchvariables Port Star Stern Bow StrafeT StrafeB Depth DepthPID.Heading
-"
-screen -dr seawolf -p watchvars -X stuff "
-"
-
-# monitor
-screen -dr seawolf -X screen -t HUD
-sleep 0.2
-screen -dr seawolf -p HUD -X stuff "cd applications
-"
-screen -dr seawolf -p HUD -X stuff "./bin/depthmonitor2
-"
-screen -dr seawolf -p HUD -X stuff "
-"
-
-# Arduino
-screen -dr seawolf -X screen -t imu_cal
-sleep 0.2
-screen -dr seawolf -p imu_cal -X "cd ~/software/external/razor-9dof-ahrs/Arduino/Razor_AHRS/
-"
-screen -dr seawolf -p imu_cal -X stuff "pwd
-"
-#screen -dr seawolf -p imu_cal -X stuff "arduino
-#"
-
-# bash
 screen -dr seawolf -X screen -t bash
 sleep 1.5
 screen -dr seawolf -p bash -X stuff "cd applications
 "
 screen -dr seawolf -p bash -X stuff "
 "
-
-# mission 
-screen -dr seawolf -X screen -t MISSION
-screen -dr seawolf -p bash -X stuff "cd mission_control
-"
-screen -dr seawolf -p bash -X stuff "
-"
-
-# cameras
-screen -dr seawolf -X screen -t cams
-sleep 0.2
-screen -dr seawolf -p bash -X stuff "sh cam_scripts/start-cams.sh
-"
-screen -dr seawolf -p bash -X stuff "
-"
-screen -dr seawolf -p bash -X stuff "cd 
-"
-
 
 screen -x
 #screen -dr seawolf -X windowlist
